4e55023fa2eec4c90e9bf91963bd9b60dd1c5be9
HasanHero1 <63868459+HasanHero1@users.noreply.github.com>
2022-11-24 23:51:33 +0600

    Update Firebase.init.js

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -4,14 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCifKS76xM85oSnX7TYzYaybYIaPAwgqgI",
-  authDomain: "ema-jon-f9a58.firebaseapp.com",
-  projectId: "ema-jon-f9a58",
-  storageBucket: "ema-jon-f9a58.appspot.com",
-  messagingSenderId: "548600315010",
-  appId: "1:548600315010:web:fb74c38ecae9dc9701489b"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
